23cbf7781ec227a822bad2f034847d481957e749
Josep Pereda <jperedaf1@upao.edu.pe>
2026-05-23 12:58:52 -0500

    refactor: implementacion del main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,40 @@
-public class Main {
-    static void main() {
+import Observer.AdminNotificationObserver;
+import Observer.EmailNotificationObserver;
+import Observer.InventoryObserver;
+import adapter.ExternalPayPalService;
+import adapter.PayPalAdapter;
+import adapter.PaymentProcessor;
+import model.Cart;
+import model.Product;
+import service.OrderService;
+import strategy.NoDiscountStrategy;
 
+public class Main {
+    static void main(String[] args) {
+        Product p1 = new Product("mario kart 8 deluxe", 60.00);
+        Product p2 = new Product("god of war 2", 60.00);
+
+        Cart cart = new Cart();
+        cart.addProduct(p1);
+        cart.addProduct(p2);
+
+
+        OrderService orderService = new OrderService();
+
+
+        orderService.addObserver(new EmailNotificationObserver());
+        orderService.addObserver(new InventoryObserver());
+        orderService.addObserver(new AdminNotificationObserver());
+
+
+        orderService.setDiscountStrategy(new NoDiscountStrategy());
+
+
+        PaymentProcessor paymentMethod = new PayPalAdapter(new ExternalPayPalService());
+
+
+        orderService.confirmarCompra(cart, paymentMethod);
     }
+
+
 }
